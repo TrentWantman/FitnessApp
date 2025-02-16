b103df03e21738aa1f324419cef62d2a9a0b73d5
Trent <Wantmantrent@gmail.com>
2025-02-16 00:18:29 -0500

    Initial Commit

---
+++ Fed/FedApp.swift
@@ -0,0 +1,17 @@
+//
+//  FedApp.swift
+//  Fed
+//
+//  Created by Trent Wantman on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct FedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
